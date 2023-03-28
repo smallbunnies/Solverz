7c53fa6c80a3984cce27b04c43e9f2facefe87bf
Ruizhi Yu <93114918+rzyu45@users.noreply.github.com>
2023-03-28 13:42:05 +0800

    refactor: replace `Sum` by `dConv` in sas_algebra.
    1. We replace `Sum` by `dConv` in sas_algebra because the `dConv` func is more intuitive in numerical computation.
    2. We can use `expand()` func to flatten the `dConv` node trees.
    3. New Index object `Slice`, as well as related DT, has been added. The docs will be supplemented in the future.
    4. The tests have been modified.
    5. The `traverse_for_DT()` func has been renamed by `dtify()`.
    6. A symbolic linspace func `dLinspace()` has been added.

--- Solverz/algebra/sas_algebra.py
+++ Solverz/algebra/sas_algebra.py
@@ -1,11 +1,10 @@
 from typing import Union, Type, Dict, Callable
 
-import numpy as np
-from sympy import Symbol, Expr, Add, Mul, Number, sin, Derivative, Sum, cos, Function, Integer
+from sympy import Symbol, Expr, Add, Mul, Number, sin, Derivative, Pow, cos, Function, Integer
 
 dfunc_mapping: Dict[Type[Expr], Callable] = {}
 
-__all__ = ['Index', 'DT', 'traverse_for_DT']
+__all__ = ['Index', 'Slice', 'DT', 'dtify', 'dLinspace', 'dConv', 'dDelta']
 
 
 class Index(Symbol):
@@ -24,37 +23,61 @@ class Index(Symbol):
         return obj
 
 
-class DT(Symbol):
-    __slots__ = ('index', 'name')
+class Slice(Symbol):
 
-    def __new__(cls, name, index: Union[int, Index], commutative=False):
-        if isinstance(index, int) and index < 0:
-            raise IndexError("Invalid DT order")
-        obj = Symbol.__new__(cls, f'{name}({index})', commutative=commutative)
-        obj.index = index
-        obj.name = f'{name}({index})'
+    def __new__(cls, start: Union[int, Index, Expr], end: Union[int, Index, Expr], commutative=False):
+        if isinstance(start, (Index, Expr)):
+            if not all([isinstance(arg, (Number, Index)) for arg in start.free_symbols]):
+                raise TypeError('Unsupported slice start!')
+        else:
+            if start < 0:
+                raise ValueError('Slice start should > 0!')
+        if isinstance(end, (Index, Expr)):
+            if not all([isinstance(arg, (Number, Index)) for arg in end.free_symbols]):
+                raise TypeError('Unsupported slice end!')
+        else:
+            if end < 0:
+                raise ValueError('Slice end should > 0!')
+        obj = Symbol.__new__(cls, f'{start}:{end}', commutative=commutative)
+        obj.start = start
+        obj.end = end
+        obj.name = f'{start}:{end}'
         return obj
 
 
-def traverse_for_DT(Node: Expr, k: [Index, Type[Expr], int]):
+class DT(Symbol):
+    __slots__ = ('index', 'name', 'symbol_name')
+
+    def __new__(cls, symbol_name, index: Union[int, Index, Slice], commutative=False):
+        if isinstance(index, int) and index < 0:
+            raise IndexError("Invalid DT order")
+        obj = Symbol.__new__(cls, f'{symbol_name}[{index}]', commutative=commutative)
+        obj.index = index
+        obj.symbol_name = symbol_name
+        obj.name = f'{symbol_name}[{index}]'
+        return obj
+
+
+def dtify(Node: Expr, k: [Index, Slice, Type[Expr], int]):
     """
     replace sympy operator by DT operators/symbols
     :param Node:
     :param k:
     :return:
     """
-    args_ = []
     if isinstance(Node, tuple(dfunc_mapping.keys())):
         return dfunc_mapping[Node.func](k, *Node.args)
     elif isinstance(Node, Symbol):
         if Node.name != 't':
             return DT(Node.name.upper(), k)
         else:  # dt of t
-            if isinstance(k, (Expr, Index)):
+            if isinstance(k, (Expr, Index)) and not isinstance(k, Slice):
                 if all([isinstance(symbol, Index) for symbol in k.free_symbols]) is False:
                     raise TypeError(f"Non-Index symbol found in Index Expression {k}!")
                 else:
                     return DT('T', k)
+            elif isinstance(k, Slice):
+                return DT('T', k)
             else:  # integer
                 if k < 0:
                     raise ValueError("DT index must be great than zero!")
@@ -79,18 +102,18 @@ def implements_dt_algebra(sym_expr: Type[Expr]):
 
 
 @implements_dt_algebra(Add)
-def dAdd(k: [Index, Type[Expr], int], *args):
+def dAdd(k: [Index, Slice, Type[Expr], int], *args):
     """
 
     :param k: DT index
     :param args: tuple of arguments
     :return:
     """
-    return Add(*[traverse_for_DT(arg, k) for arg in tuple(args)])
+    return Add(*[dtify(arg, k) for arg in tuple(args)])
 
 
 @implements_dt_algebra(Mul)
-def dMul(k: [Index, Type[Expr], int], *args):
+def dMul(k: [Index, Slice, Type[Expr], int], *args):
     """
     distinguish between scalar production and Expr/Symbol production, supports only two operands
     :param k: DT index
@@ -100,34 +123,29 @@ def dMul(k: [Index, Type[Expr], int], *args):
     if isinstance(args[0], Number):
         # assumes that scalar Number is always in front of Symbol
         # for example a=x*3 and a=3*x all derive a=3*x
-        return Mul(args[0], traverse_for_DT(Mul(*args[1:]), k))
+        return Mul(args[0], dtify(Mul(*args[1:]), k))
     elif all([isinstance(arg, tuple(list(dfunc_mapping.keys()) + [Symbol])) for arg in args]):
-        if isinstance(k, (Expr, Index)):
-            if all([isinstance(symbol, Index) for symbol in k.free_symbols]):
-                sequence_new = np.max([symbol.sequence for symbol in k.free_symbols]) + 1
-            else:
+        if isinstance(k, (Expr, Index)) and not isinstance(k, Slice):
+            if any([not isinstance(symbol, Index) for symbol in k.free_symbols]):
                 raise TypeError(f"Non-Index symbol found in Index Expression {k}!")
-            m = Index('k', sequence=sequence_new)
-            if sequence_new == 0:
-                # Extract X(k)Y(0) and X(0)Y(k) from sigma(X(m)Y(k-m)) if k.sequence == -1,
-                # since X(k) and Y(k) are unknowns DT orders in this case.
-                return Sum(Mul(traverse_for_DT(args[0], m), traverse_for_DT(Mul(*args[1:]), k - m)), (m, 1, k - 1)) + \
-                    traverse_for_DT(args[0], 0) * traverse_for_DT(Mul(*args[1:]), k) + \
-                    traverse_for_DT(args[0], k) * traverse_for_DT(Mul(*args[1:]), 0)
+            return dConv(dtify(args[0], Slice(0, k)), dtify(Mul(*args[1:]), Slice(0, k)))
+        elif isinstance(k, Slice):
+            if k.start >= 1:
+                temp = dConv(dtify(args[0], Slice(0, k.end - k.start)), dtify(Mul(*args[1:]), k))
+                for i in range(k.start):
+                    temp = temp + dtify(args[0], Slice(k.start - i, k.end - i)) * dtify(Mul(*args[1:]), i)
+                return temp
             else:
-                return Sum(Mul(traverse_for_DT(args[0], m), traverse_for_DT(Mul(*args[1:]), k - m)), (m, 0, k))
+                return dConv(dtify(args[0], k), dtify(Mul(*args[1:]), k))
         else:  # integer
-            if k > 1:
-                m = Index('k', sequence=0)
-                return Sum(Mul(traverse_for_DT(args[0], m), traverse_for_DT(Mul(*args[1:]), k - m)), (m, 1, k - 1)) + \
-                    traverse_for_DT(args[0], 0) * traverse_for_DT(Mul(*args[1:]), k) + \
-                    traverse_for_DT(args[0], k) * traverse_for_DT(Mul(*args[1:]), 0)
+            if k >= 1:
+                return dConv(dtify(args[0], Slice(0, k)), dtify(Mul(*args[1:]), Slice(0, k)))
             else:  # k==0
-                return traverse_for_DT(args[0], 0) * traverse_for_DT(Mul(*args[1:]), 0)
+                return dtify(args[0], 0) * dtify(Mul(*args[1:]), 0)
 
 
 @implements_dt_algebra(sin)
-def dSin(k: [Index, Type[Expr]], *args):
+def dSin(k: [Index, Slice, Type[Expr]], *args):
     """
 
     :param k:
@@ -136,19 +154,30 @@ def dSin(k: [Index, Type[Expr]], *args):
     """
     if len(args) > 1:
         raise ValueError(f'Sin supports one operand while {len(args)} input!')
-    if isinstance(k, (Expr, Index)):
-        if all([isinstance(symbol, Index) for symbol in k.free_symbols]):
-            sequence_new = np.max([symbol.sequence for symbol in k.free_symbols]) + 1
-        else:
+    if isinstance(k, (Expr, Index)) and not isinstance(k, Slice):
+        if any([not isinstance(symbol, Index) for symbol in k.free_symbols]):
             raise TypeError(f"Non-Index symbol found in Index Expression {k}!")
-        m = Index('k', sequence=sequence_new)
-        return Sum(DT('Psi', m) * (k - m) / k * traverse_for_DT(args[0], k - m), (m, 0, k - 1))
+        return dConv((k - dLinspace(0, k - 1)) / k * DT('Psi', Slice(0, k - 1)), dtify(args[0], Slice(1, k)))
+    elif isinstance(k, Slice):
+        if k.start >= 2:
+            Psi1 = DT('Psi', Slice(0, k.end - k.start))
+            Psi2 = DT('Psi', Slice(1, k.end - 1))
+            return dConv((k.end - dLinspace(0, k.end - k.start)) / k.end * Psi1, dtify(args[0], k)) + \
+                dConv((k.end - dLinspace(1, k.end - 1)) / k.end * Psi2, dtify(args[0], 1))
+        elif k.start == 1:
+            Psi = DT('Psi', Slice(0, k.end - 1))
+            return dConv((k.end - dLinspace(0, k.end - 1)) / k.end * Psi, dtify(args[0], k))
+        else:
+            Phi = DT('Phi', 0)
+            Psi = DT('Psi', Slice(0, k.end))
+            return dDelta(Slice(0, k.end)) * Phi + dConv((k.end - dLinspace(0, k.end)) / k.end * Psi,
+                                                         dtify(args[0], Slice(0, k.end))) * (
+                    1 - dDelta(Slice(0, k.end)))
     else:  # integer
         if k > 1:
-            m = Index('k', sequence=0)
-            return Sum(DT('Psi', m) * (k - m) / k * traverse_for_DT(args[0], k - m), (m, 0, k - 1))
+            return dConv((k - dLinspace(0, k - 1)) / k * DT('Psi', Slice(0, k - 1)), dtify(args[0], Slice(1, k)))
         elif k == 1:
-            return DT('Psi', 0) * traverse_for_DT(args[0], 1)
+            return DT('Psi', 0) * dtify(args[0], 1)
         elif k == 0:
             return DT('Phi', 0)
         else:
@@ -156,7 +185,7 @@ def dSin(k: [Index, Type[Expr]], *args):
 
 
 @implements_dt_algebra(cos)
-def dCos(k: [Index, Type[Expr], int], *args):
+def dCos(k: [Index, Slice, Type[Expr], int], *args):
     """
 
     :param k:
@@ -165,23 +194,45 @@ def dCos(k: [Index, Type[Expr], int], *args):
     """
     if len(args) > 1:
         raise ValueError(f'Sin supports one operand while {len(args)} input!')
-    if isinstance(k, (Expr, Index)):
-        if all([isinstance(symbol, Index) for symbol in k.free_symbols]):
-            sequence_new = np.max([symbol.sequence for symbol in k.free_symbols]) + 1
-        else:
+    if isinstance(k, (Expr, Index)) and not isinstance(k, Slice):
+        if any([not isinstance(symbol, Index) for symbol in k.free_symbols]):
             raise TypeError(f"Non-Index symbol found in Index Expression {k}!")
-        m = Index('k', sequence=sequence_new)
-        return -Sum(DT('Phi', m) * (k - m) / k * traverse_for_DT(args[0], k - m), (m, 0, k - 1))
+        return -dConv(((k - dLinspace(0, k - 1)) / k) * DT('Phi', Slice(0, k - 1)),
+                      dtify(args[0], Slice(1, k)))
+    elif isinstance(k, Slice):
+        if k.start >= 2:
+            Phi1 = DT('Phi', Slice(0, k.end - k.start))
+            Phi2 = DT('Phi', Slice(1, k.end - 1))
+            return -dConv((k.end - dLinspace(0, k.end - k.start)) / k.end * Phi1, dtify(args[0], k)) - \
+                dConv((k.end - dLinspace(1, k.end - 1)) / k.end * Phi2, dtify(args[0], 1))
+        elif k.start == 1:
+            Phi = DT('Phi', Slice(0, k.end - 1))
+            return -dConv((k.end - dLinspace(0, k.end - 1)) / k.end * Phi, dtify(args[0], k))
+        else:
+            Psi = DT('Psi', Slice(0, k.end - 1))
+            Phi = DT('Phi', 0)
+            return dDelta(k.end) * Phi - dConv((k.end - dLinspace(0, k.end - 1)) / k.end * Psi,
+                                               dtify(args[0], Slice(1, k.end))) * (1 - dDelta(k.end))
     else:  # integer
         if k > 1:
-            m = Index('k', sequence=0)
-            return -Sum(DT('Phi', m) * (k - m) / k * traverse_for_DT(args[0], k - m), (m, 0, k - 1))
+            return -dConv((k - dLinspace(0, k - 1)) / k * DT('Phi', Slice(0, k - 1)), dtify(args[0], Slice(1, k)))
         elif k == 1:
-            return -DT('Phi', 0) * traverse_for_DT(args[0], 1)
+            return -DT('Phi', 0) * dtify(args[0], 1)
         elif k == 0:
             return DT('Psi', 0)
         else:
             raise ValueError(f'DT index must be great than zero!')
+
+
+@implements_dt_algebra(Pow)
+def dPow(k: [Index, Slice, Type[Expr], int], *args):
+    """
+
+    :param k:
+    :param args
+    :return:
+    """
+    pass
 
 
 @implements_dt_algebra(Derivative)
@@ -192,7 +243,7 @@ def dDerivative(k: [Index, Type[Expr], int], *args):
     :param args:
     :return:
     """
-    return (k + 1) * traverse_for_DT(args[0], k + 1)
+    return (k + 1) * dtify(args[0], k + 1)
 
 
 class dDelta(Function):
@@ -207,7 +258,76 @@ class dDelta(Function):
                 return 1
             else:
                 return 0
-        if args[0].is_integer is False:
-            # allow None type Integer assumption, i.e. is_integer == None, which is the default integer assumption of
-            # symbols created by sp.symbols()
-            raise TypeError("Inputs should be an integer.")
+        # if args[0].is_integer is False:
+        # allow None type Integer assumption, i.e. is_integer == None, which is the default integer assumption of
+        # symbols created by sp.symbols()
+        # raise TypeError("Inputs should be an integer.")
+
+
+class dConv(Function):
+    is_commutative = False
+    is_dConv = True
+
+    @classmethod
+    def eval(cls, *args):
+        pass
+
+    def __mul__(self, other):
+        args = list(self.args)
+        args[-1] = Mul(args[-1] * other)
+        return self.func(*args)
+
+    def __rmul__(self, other):
+        args = list(self.args)
+        args[0] = Mul(other * args[0])
+        return self.func(*args)
+
+    def _eval_expand_func(self, **hints):
+        # traverse to flatten dConv tree
+        args = self.args
+        i = -1
+        for arg in args:
+            i = i + 1
+            if arg.has(dConv):
+                if arg.is_Add:
+                    x, y = arg.as_two_terms()
+                    temp_args1 = list(args)
+                    temp_args1[i] = x
+                    temp_args2 = list(args)
+                    temp_args2[i] = y
+                    return dConv(*temp_args1).expand(func=True, mul=False) + dConv(*temp_args2).expand(func=True,
+                                                                                                       mul=False)
+                elif arg.func == dConv:
+                    # extract the arguments of sub-dConv nodes
+                    if i == 0:
+                        return self.func(*(list(arg.args) + list(args[1:]))).expand(func=True, mul=False)
+                    elif i == len(args) - 1:
+                        return self.func(*(list(args[:-1]) + list(arg.args))).expand(func=True, mul=False)
+                    else:
+                        return self.func(*(list(args[0:i - 1]) + list(arg.args) + list(args[0:i + 1]))).expand(
+                            func=True, mul=False)
+                # elif arg.is_Mul:
+                    # deprecated by overriding multiplication
+
+        return self
+
+
+class dLinspace(Function):
+    is_commutative = False
+    is_dLinspace = True
+
+    @classmethod
+    def eval(cls, *args):
+        pass
+
+
+# TODO : Extract k-th order terms from conv.
+def nConv(a, b, *args):
+    """
+    numerical convolution of vectors using loop or FFT
+    :param a:
+    :param b:
+    :param args:
+    :return:
+    """
+    pass
--- Solverz/algebra/tests/test_algebra.py
+++ Solverz/algebra/tests/test_algebra.py
@@ -3,47 +3,111 @@ from sympy import Derivative, Sum
 from sympy.abc import x, y, z, t
 
 from Solverz.algebra.sas_algebra import *
-from Solverz.algebra.sas_algebra import dDelta
+from Solverz.algebra.sas_algebra import dDelta, dConv
 
 k = Index('k')
 k0 = Index('k', sequence=0)
-k1 = Index('k', sequence=1)
+Xk = DT('X', k)
+X0 = DT('X', 0)
+X1k = DT('X', Slice(1, k))
+Yk = DT('Y', k)
+Y0 = DT('Y', 0)
+Zk = DT('Z', k)
+Z0 = DT('Z', 0)
+Y1k = DT('Y', Slice(1, k))
+Z1k = DT('Z', Slice(1, k))
+X0k = DT('X', Slice(0, k))
+X0k1 = DT('X', Slice(0, k - 1))
+Y0k = DT('Y', Slice(0, k))
+Y0k1 = DT('Y', Slice(0, k - 1))
+Z0k = DT('Z', Slice(0, k))
+Psi0k1 = DT('Psi', Slice(0, k - 1))
+Phi0k1 = DT('Phi', Slice(0, k - 1))
+Psi0k = DT('Psi', Slice(0, k))
+Phi0k = DT('Phi', Slice(0, k))
+Psi0 = DT('Psi', 0)
+Phi0 = DT('Phi', 0)
+Lin0k1 = dLinspace(0, k - 1)
+Lin0k = dLinspace(0, k)
+X03 = DT('X', Slice(0, 3))
+Y03 = DT('Y', Slice(0, 3))
+Z03 = DT('Z', Slice(0, 3))
 
 
 def test_dt_algebra():
-    assert traverse_for_DT(x + y, Index('k')) == DT('X', k) + DT('Y', k)
-    assert traverse_for_DT(3 * x, Index('k')) == 3 * DT('X', k)
-    assert traverse_for_DT(x * y, Index('k')) == Sum(DT('X', k0) * DT('Y', k - k0), (k0, 1, k - 1)) + \
-           + DT('X', 0) * DT('Y', k) + DT('X', k) * DT('Y', 0)
-    assert traverse_for_DT(z, Index('k')) == DT('Z', k)
-    assert traverse_for_DT(z - x, Index('k')) == DT('Z', k) - DT('X', k)
-    assert traverse_for_DT(z - x * y, Index('k')) == DT('Z', k) + (-1) * \
-           (DT('X', 0) * DT('Y', k) + DT('X', k) * DT('Y', 0) + Sum(DT('X', k0) * DT('Y', k - k0), (k0, 1, k - 1)))
-    assert traverse_for_DT(z - 3 * x * y, Index('k')) == DT('Z', k) + (-3) * \
-           (DT('X', 0) * DT('Y', k) + DT('X', k) * DT('Y', 0) + Sum(DT('X', k0) * DT('Y', k - k0), (k0, 1, k - 1)))
-    assert traverse_for_DT(z - x * y * z, Index('k')) == DT('Z', k) + (-1)*(DT('X', 0)*(DT('Y', 0)*DT('Z', k) + DT('Y', k)*DT('Z', 0) + Sum(DT('Y', k0)*DT('Z', k - k0), (k0, 1, k - 1))) + DT('X', k)*DT('Y', 0)*DT('Z', 0) + Sum(DT('X', k0)*Sum(DT('Y', k1)*DT('Z', k - k0 - k1), (k1, 0, k - k0)), (k0, 1, k - 1)))
-    assert traverse_for_DT(z - x * y * z, 3) == DT('Z', 3) + (-1) * (DT('X', 0)*(DT('Y', 0)*DT('Z', 3) + DT('Y', 3)*DT('Z', 0) + Sum(DT('Y', k0)*DT('Z', 3 - k0), (k0, 1, 2))) + DT('X', 3)*DT('Y', 0)*DT('Z', 0) + Sum(DT('X', k0)*Sum(DT('Y', k1)*DT('Z', -k0 - k1 + 3), (k1, 0, 3 - k0)), (k0, 1, 2)))
-
-    assert traverse_for_DT(Derivative(y, t), Index('k')) == (k + 1) * DT('Y', k + 1)
-    assert traverse_for_DT(sp.sin(x), Index('k')) == Sum((k - k0) / k * DT('Psi', k0) * DT('X', k - k0), (k0, 0, k - 1))
-    assert traverse_for_DT(sp.sin(x), 0) == DT('Phi', 0)
-    assert traverse_for_DT(sp.sin(x), 1) == DT('Psi', 0) * DT('X', 1)
-    assert traverse_for_DT(sp.cos(x), Index('k')) == -Sum((k - k0) / k * DT('Phi', k0) * DT('X', k - k0),
-                                                          (k0, 0, k - 1))
-    assert traverse_for_DT(sp.cos(x), 0) == DT('Psi', 0)
-    assert traverse_for_DT(sp.cos(x), 1) == -DT('Phi', 0) * DT('X', 1)
-
-    assert traverse_for_DT(sp.sin(x * y + z), Index('k')) == Sum(
-        (k - k0) / k * DT('Psi', k0) * (DT('Z', k - k0) + Sum(DT('X', k1) * DT('Y', k - k0 - k1), (k1, 0, k - k0))),
-        (k0, 0, k - 1))
-    assert traverse_for_DT(sp.Integer(5), Index('k')) == 5 * dDelta(k)
-    assert traverse_for_DT(sp.Integer(5), 0) == 5
-    assert traverse_for_DT(sp.Integer(5), 1) == 0
-    assert traverse_for_DT(t, Index('k')) == DT('T', Index('k'))
-    assert traverse_for_DT(t, 0) == 0
-    assert traverse_for_DT(t, 1) == 1
-    assert traverse_for_DT(sp.sin(t), 0) == DT('Phi', 0)
-    assert traverse_for_DT(sp.sin(t), Index('k')) == Sum((k - k0) * DT('Psi', k0) * DT('T', k - k0) / k, (k0, 0, k - 1))
-
-
-
+    assert dtify(x + y, k) == Xk + Yk
+    assert dtify(x * y, k) == dConv(X0k, Y0k)
+    assert dtify(x * y, Slice(0, k)) == dConv(X0k, Y0k)
+    assert dtify(x * y, Slice(1, k)) == dConv(X0k1, Y1k) + X1k * Y0
+    assert dtify(x * y * z, k) == dConv(X0k, dConv(Y0k, DT('Z', Slice(0, k))))
+    assert dtify(x * y * z, k).expand(func=True, mul=False) == dConv(X0k, Y0k, DT('Z', Slice(0, k)))
+    assert dtify(z - x * y, k) == DT('Z', k) - dConv(X0k, Y0k)
+    assert dtify(z * (x + y), k) == dConv(DT('Z', Slice(0, k)), X0k + Y0k)
+    assert dtify(x + y, 3) == DT('X', 3) + DT('Y', 3)
+    assert dtify(x * y, 3) == dConv(X03, Y03)
+    assert dtify(x * y * z, 3) == dConv(X03, dConv(Y03, Z03))
+    assert dtify(x * y * z, 3).expand(func=True, mul=False) == dConv(X03, Y03, Z03)
+    assert dtify(z - x * y, 3) == DT('Z', 3) - dConv(X03, Y03)
+    assert dtify(z * (x + y), 3) == dConv(Z03, X03 + Y03)
+    assert dtify(x + y, 0) == X0 + Y0
+    assert dtify(x * y, 0) == X0 * Y0
+    assert dtify(x * y * z, 0) == X0 * Y0 * Z0
+    assert dtify(z - x * y, 0) == Z0 - X0 * Y0
+    assert dtify(z * (x + y), 0) == Z0 * (X0 + Y0)
+    assert dtify(sp.sin(x), k) == dConv((k - Lin0k1) * Psi0k1 / k, X1k)
+    assert dtify(sp.sin(x), 2) == dConv((2 - dLinspace(0, 1)) * DT('Psi', Slice(0, 1)) / 2, DT('X', Slice(1, 2)))
+    assert dtify(sp.sin(x), 1) == Psi0 * DT('X', 1)
+    assert dtify(sp.sin(x), 0) == Phi0
+    assert dtify(sp.sin(x * y), k) == dConv((k - Lin0k1) * Psi0k1 / k, dConv(X0k1, Y1k) + X1k * Y0)
+    expr1 = dtify(sp.sin(x * y), k)
+    assert \
+        expr1.expand(func=True, mul=False) == \
+        dConv((k - Lin0k1) * Psi0k1 / k, X0k1, Y1k) + dConv((k - Lin0k1) * Psi0k1 / k, X1k * Y0)
+    assert dtify(sp.sin(x * y), 2) == \
+           dConv((2 - dLinspace(0, 1)) * DT('Psi', Slice(0, 1)) / 2,
+                 dConv(DT('X', Slice(0, 1)), DT('Y', Slice(1, 2))) + DT('X', Slice(1, 2)) * Y0)
+    assert dtify(sp.sin(x * y), 1) == Psi0 * dConv(DT('X', Slice(0, 1)), DT('Y', Slice(0, 1)))
+    assert dtify(sp.sin(x * y), 0) == Phi0
+    assert dtify(sp.cos(x), k) == -dConv((k - Lin0k1) * Phi0k1 / k, X1k)
+    assert dtify(sp.cos(x), 2) == -dConv((2 - dLinspace(0, 1)) * DT('Phi', Slice(0, 1)) / 2, DT('X', Slice(1, 2)))
+    assert dtify(sp.cos(x), 1) == -Phi0 * DT('X', 1)
+    assert dtify(sp.cos(x), 0) == Psi0
+    assert dtify(sp.cos(x * y), k) == -dConv((k - Lin0k1) * Phi0k1 / k, dConv(X0k1, Y1k) + X1k * Y0)
+    assert dtify(sp.cos(x * y), k).expand(func=True, mul=False) == \
+           (-1) * (dConv((k - Lin0k1) * Phi0k1 / k, X0k1, Y1k) +
+                   dConv((k - Lin0k1) * Phi0k1 / k, X1k * Y0))
+    assert dtify(sp.cos(x * y), 2) \
+           == -dConv((2 - dLinspace(0, 1)) * DT('Phi', Slice(0, 1)) / 2,
+                     dConv(DT('X', Slice(0, 1)), DT('Y', Slice(1, 2))) +
+                     DT('X', Slice(1, 2)) * Y0)
+    assert dtify(sp.cos(x * y), 1) == -Phi0 * dConv(DT('X', Slice(0, 1)), DT('Y', Slice(0, 1)))
+    assert dtify(sp.cos(x * y), 0) == Psi0
+    expr2 = sp.cos(x * y + x * z)
+    assert dtify(expr2, k) == \
+           -dConv((k - Lin0k1) * Phi0k1 / k, dConv(X0k1, Y1k) + X1k * Y0 + dConv(X0k1, Z1k) + X1k * Z0)
+    assert dtify(expr2, k).expand(func=True, mul=False) == \
+           (-1) * (dConv((k - Lin0k1) * Phi0k1 / k, X0k1, Y1k) + dConv((k - Lin0k1) * Phi0k1 / k, X1k * Y0) +
+                   dConv((k - Lin0k1) * Phi0k1 / k, X0k1, Z1k) + dConv((k - Lin0k1) * Phi0k1 / k, X1k * Z0))
+    expr3 = x * sp.sin(y)
+    assert dtify(expr3, k) == \
+           dConv(X0k, dConv((k - Lin0k) * Psi0k / k, Y0k * (1 - dDelta(Slice(0, k)))) + dDelta(Slice(0, k)) * Phi0)
+    expr4 = x * sp.sin(x * y)
+    assert dtify(expr4, k) == \
+           dConv(X0k, dConv((k - Lin0k) * Psi0k / k, dConv(X0k, Y0k * (1 - dDelta(Slice(0, k))))) + dDelta(Slice(0, k)) * Phi0)
+    assert dtify(expr4, k).expand(func=True, mul=False) == \
+           dConv(X0k, (k - Lin0k) * Psi0k / k, X0k, Y0k * (1 - dDelta(Slice(0, k)))) + \
+           dConv(X0k, dDelta(Slice(0, k)) * Phi0)
+    expr5 = (x + x * z) * (x + y * z)
+    assert dtify(expr5, k) == dConv(X0k + dConv(X0k, Z0k), X0k + dConv(Y0k, Z0k))
+    assert dtify(expr5, k).expand(func=True, mul=False) == \
+           dConv(X0k, X0k) + dConv(X0k, Y0k, Z0k) + dConv(X0k, Z0k, X0k) + dConv(X0k, Z0k, Y0k, Z0k)
+    assert dtify(sp.sin(t), Index('k')) == dConv((k - Lin0k1) * Psi0k1 / k, DT('T', Slice(1, k)))
+    assert dtify(Derivative(y, t), Index('k')) == (k + 1) * DT('Y', k + 1)
+    assert dtify(sp.Integer(5), Index('k')) == 5 * dDelta(k)
+    assert dtify(sp.Integer(5), 0) == 5
+    assert dtify(sp.Integer(5), 1) == 0
+    assert dtify(t, Index('k')) == DT('T', Index('k'))
+    assert dtify(t, 0) == 0
+    assert dtify(t, 1) == 1
+    assert dtify(sp.sin(t), 0) == DT('Phi', 0)
+    assert dtify(sp.sin(t), Index('k')) == dConv((k - Lin0k1) * Psi0k1 / k, DT('T', Slice(1, k)))
