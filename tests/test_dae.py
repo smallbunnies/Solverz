603c6d3f9e08fd022deb64829c7616450c7dc0f5
Ruizhi Yu <rz.yu@foxmail.com>
2025-03-17 21:56:12 +0800

    test: add ode15s test

--- tests/test_dae.py
+++ tests/test_dae.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from Solverz import Eqn, Ode, DAE, Rodas, implicit_trapezoid, Opt, made_numerical, Model, Var
+from Solverz import Eqn, Ode, DAE, Rodas, implicit_trapezoid, Opt, made_numerical, Model, Var, ode15s
 
 m = Model()
 
@@ -12,7 +12,7 @@ m.f = Ode(name='f', f=-m.x ** 3 + 0.5 * m.y ** 2, diff_var=m.x)
 m.g = Eqn(name='g', eqn=m.x ** 2 + m.y ** 2 - 2)
 dae, y0 = m.create_instance()
 
-ndae = made_numerical(dae, y0)
+ndae = made_numerical(dae, y0, sparse=True)
 
 df = pd.read_excel('tests/dae_test.xlsx',
                    sheet_name=None,
@@ -30,6 +30,10 @@ sol_rodas_dense = Rodas(ndae,
                         Opt(hinit=0.1))
 sol_trape = implicit_trapezoid(ndae, [0, 20], y0, Opt(step_size=0.1))
 
+sol_ode15s = ode15s(ndae,
+                    np.linspace(0, 20, 201),
+                    y0)
+
 sol_bench = Rodas(ndae,
                   np.linspace(0, 20, 201),
                   y0,
@@ -43,3 +47,7 @@ def test_dae():
     xy_bench1 = np.asarray(df['rodas_dense'])
     assert np.max(np.abs(xy_bench1 - sol_rodas_dense.Y)) < 1e-8
     assert np.max(np.abs(sol_bench.Y.array - sol_trape.Y.array)) < 7e-4
+    np.testing.assert_allclose(sol_ode15s.Y.array,
+                               np.asarray(df['ode15s']),
+                               rtol=1e-6,
+                               atol=1e-6)
