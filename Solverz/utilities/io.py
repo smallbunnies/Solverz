b2efd6c35ed5eb06818a0da6b284fa250f23f12b
Ruizhi Yu <rz.yu@foxmail.com>
2025-05-22 19:23:00 +0800

    feat: add matlab result saver (#109)

--- Solverz/utilities/io.py
+++ Solverz/utilities/io.py
@@ -1,6 +1,7 @@
 import dill
 import numpy as np
 import pandas as pd
+from scipy.io import savemat
 from Solverz.variable.variables import TimeVars
 from Solverz.solvers.solution import aesol, daesol
 
@@ -15,7 +16,7 @@ def load(filename: str):
         return dill.load(file)
 
 
-def save_result(sol: aesol | daesol, name: str, dir: str = None):
+def save_result(sol: aesol | daesol, name: str, dir: str = None, f='xlsx'):
     if dir is None:
         dir = ''
     else:
@@ -24,35 +25,58 @@ def save_result(sol: aesol | daesol, name: str, dir: str = None):
     if isinstance(sol, daesol):
         T = sol.T
         Y = sol.Y
-        T = pd.DataFrame({'T': T})
-        Y_dict = dict()
-        for var in Y.var_list:
-            tempd = dict()
-            for j in range(Y.var_size[var]):
-                tempd[var + f'_{j}'] = Y[var][:, j]
-            Y_dict[var] = pd.DataFrame(tempd)
-        with pd.ExcelWriter(dir + f'{name}.xlsx', engine='openpyxl') as writer:
-            # Write each DataFrame to a different sheet
-            T.to_excel(writer, sheet_name='Time')
-            for var, df in Y_dict.items():
-                df.to_excel(writer, sheet_name=var)
-        if sol.ie is not None:
-            if len(sol.ie) > 0:
-                te = pd.DataFrame({'te': sol.te})
-                ie = pd.DataFrame({'ie': sol.ie})
-                ye = sol.ye
-                ye_d = dict()
-                for var in ye.var_list:
-                    tempd = dict()
-                    for j in range(ye.var_size[var]):
-                        tempd[var + f'_{j}'] = ye[var][:, j]
-                    ye_d[var] = pd.DataFrame(tempd)
-                with pd.ExcelWriter(dir + f'event_in_{name}.xlsx', engine='openpyxl') as writer:
-                    # Write each DataFrame to a different sheet
-                    te.to_excel(writer, sheet_name='Event Time')
-                    ie.to_excel(writer, sheet_name='Event Index')
-                    for var, df in ye_d.items():
-                        df.to_excel(writer, sheet_name=var)
+
+        if f == 'xlsx':
+            Y_dict = dict()
+            for var in Y.var_list:
+                tempd = dict()
+                for j in range(Y.var_size[var]):
+                    tempd[var + f'_{j}'] = Y[var][:, j]
+                Y_dict[var] = pd.DataFrame(tempd)
+
+            T = pd.DataFrame({'T': T})
+            with pd.ExcelWriter(dir + f'{name}.xlsx', engine='openpyxl') as writer:
+                # Write each DataFrame to a different sheet
+                T.to_excel(writer, sheet_name='Time')
+                for var, df in Y_dict.items():
+                    df.to_excel(writer, sheet_name=var)
+
+            if sol.ie is not None:
+                if len(sol.ie) > 0:
+                    te = pd.DataFrame({'te': sol.te})
+                    ie = pd.DataFrame({'ie': sol.ie})
+                    ye = sol.ye
+                    ye_d = dict()
+                    for var in ye.var_list:
+                        tempd = dict()
+                        for j in range(ye.var_size[var]):
+                            tempd[var + f'_{j}'] = ye[var][:, j]
+                        ye_d[var] = pd.DataFrame(tempd)
+                    with pd.ExcelWriter(dir + f'event_in_{name}.xlsx', engine='openpyxl') as writer:
+                        # Write each DataFrame to a different sheet
+                        te.to_excel(writer, sheet_name='Event Time')
+                        ie.to_excel(writer, sheet_name='Event Index')
+                        for var, df in ye_d.items():
+                            df.to_excel(writer, sheet_name=var)
+
+        elif f == 'matlab':
+            sol_d = dict()
+            sol_d['Time'] = T
+
+            sol_d['Y'] = dict()
+            for var in Y.var_list:
+                sol_d['Y'][var] = Y[var]
+
+            if sol.ie is not None:
+                if len(sol.ie) > 0:
+                    sol_d['ye'] = dict()
+                    sol_d['te'] = sol.te
+                    sol_d['ie'] = sol.ie
+                    for var in sol.ye.var_list:
+                        sol_d['ye'][var] = sol.ye[var]
+
+            savemat(dir + f'{name}.mat', {f'sol_{name}': sol_d})
+
     elif isinstance(sol, aesol):
         y = sol.y
         y_dict = dict()
