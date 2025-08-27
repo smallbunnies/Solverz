071728bd40dd6adfff4c324d4cd2e78ce766e334
Ruizhi Yu <rz.yu@foxmail.com>
2025-08-27 13:22:26 +0800

    refactor: update linear solver

--- Solverz/solvers/laesolver.py
+++ Solverz/solvers/laesolver.py
@@ -3,6 +3,11 @@ from typing import Union
 import numpy as np
 from scipy.sparse import csc_array, csc_matrix, csr_array, csr_matrix, linalg as sla
 
+# from scikits import umfpack
+# umfpack is slow compared with superlu on Apple M4 with MACOS 15.6.1, scikits-umfpack 0.4.2, and suite-sparse 7.10.3.
+# Also, it was found that umfpack was not accurate enough, causing non-convergence issues.
+
+splu = sla.splu
 
 def solve(A, b):
     if isinstance(A, (csc_array, csc_matrix, csr_array, csr_matrix)):
@@ -30,7 +35,12 @@ class dense_decomposition:
 class sp_decomposition:
     def __init__(self,
                  A: Union[(csc_array, csc_matrix)]):
-        self.lu_solve = sla.factorized(A)
+        self.splu = splu(A)
+        self.perm_r = self.splu.perm_r
+        self.perm_c = self.splu.perm_c
+        self.L = self.splu.L
+        self.U = self.splu.U
+        self.nnz = self.splu.nnz
 
     def solve(self, b):
-        return self.lu_solve(b)
+        return self.splu.solve(b)
